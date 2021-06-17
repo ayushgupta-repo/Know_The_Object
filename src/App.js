6314c384b50a50dd14bc89087b8b3789c364ef9f
ayushgupta-repo <19ce037@charusat.edu.in>
2021-06-17 11:04:19 +0530

    Day 5:Imported react and tensorflow(object detection) modules for the project.

---
+++ src/App.js
@@ -0,0 +1,7 @@
+// Importing dependencies and modules
+
+import React, { useRef, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import Webcam from "react-webcam";
+import "./App.css";

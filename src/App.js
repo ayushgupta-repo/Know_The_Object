5b523140a13d4c95f4764e5073e45b0aa0f196a4
ayushgupta-repo <19ce037@charusat.edu.in>
2021-06-20 17:04:58 +0530

    Day 7: Created webcam ref, main func with asynchronous function.

--- src/App.js
+++ src/App.js
@@ -6,3 +6,19 @@ import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
 import "./App.css";
 import { drawRect } from './utilities';
+
+function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  // Main function
+  const runCoco = async () => {
+
+    // Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 10);
+  };
+
+  
+}
